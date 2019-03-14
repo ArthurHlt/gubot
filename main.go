20d2863ee46e816641689ca69f0fb747567232db
ArthurHlt <arthur.halet@orange.com>
2019-03-14 16:41:13 +0100

    go back to slack

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 	// adapters
 	_ "github.com/ArthurHlt/gubot/adapter/shell"
-	// _ "github.com/ArthurHlt/gubot/adapter/slack"
-	_ "github.com/ArthurHlt/gubot/adapter/tts_watson"
+	_ "github.com/ArthurHlt/gubot/adapter/slack"
+	// _ "github.com/ArthurHlt/gubot/adapter/tts_watson"
 	// _ "github.com/ArthurHlt/gubot/adapter/mattermost_user"
 
 	// scripts
